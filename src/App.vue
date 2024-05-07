7c9bdfcf7bd054c7cb055ca65105a26b4358ca45
Viktor Petruk <damnnn261@gmail.com>
2024-05-07 06:16:16 +0300

    final setup, add the pinia stuff in our project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
-<template>
-	<div></div>
-</template>
-
 <script setup>
+import { onMounted, ref } from 'vue'
 
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
+import TrendingUp from 'vue-material-design-icons/TrendingUp.vue';
+import Television from 'vue-material-design-icons/Television.vue';
+import MovieOutline from 'vue-material-design-icons/MovieOutline.vue';
+import Plus from 'vue-material-design-icons/Plus.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+
+import { useMovieStore } from './stores/movies';
+import { storeToRefs } from 'pinia';
+const useMovie = useMovieStore();
+const { movie, showFullWideo } = storeToRefs(useMovie);
 </script>
+
+<template>
+	<div>
+
+	</div>
+</template>
